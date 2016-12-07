4e33f76eaf9717a2b53447cd1d69765f5d6adf06
wu <1004691415@qq.com>
2016-12-07 13:52:40 +0800

    joe1

--- Caps/src/main/java/edu/iss/caps/model/Course.java
+++ Caps/src/main/java/edu/iss/caps/model/Course.java
@@ -31,7 +31,7 @@ public class Course {
 	private int currentEnrollment;
 	@Column(name="Size")
 	private int size;
-//linda
+
 	@OneToOne
     @JoinColumn(name="LecturerId") 
 	private LecturerDetail lecturerDetails;
