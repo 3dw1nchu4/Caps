d04cf50245102e69590a33c9800df900d540a24c
wu <1004691415@qq.com>
2016-12-08 20:18:59 +0800

    studentcontrol

--- Caps/src/main/java/edu/iss/caps/controller/StudentController.java
+++ Caps/src/main/java/edu/iss/caps/controller/StudentController.java
@@ -1,69 +1,153 @@
 package edu.iss.caps.controller;
 
-	import java.util.ArrayList;
-	import java.util.List;
+import java.util.List;
 
-	import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
+//import javax.validation.Valid;
 
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.stereotype.Controller;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.bind.annotation.RequestMethod;
-	import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-	import edu.iss.caps.model.Course;
-	import edu.iss.caps.model.Enrolment;
-	import edu.iss.caps.service.CourseService;
-	import edu.iss.caps.service.EnrolmentService;
+import edu.iss.caps.model.Course;
+import edu.iss.caps.model.Enrolment;
+import edu.iss.caps.model.StudentDetail;
+import edu.iss.caps.service.CourseService;
+//import edu.iss.caps.service.StudentService;
+import edu.iss.caps.service.EnrolmentService;
+import edu.iss.caps.service.StudentService;
 
-		@Controller
-		@RequestMapping(value = "/Student")
+
+
+@RequestMapping(value="/Course")
+@Controller
+public class StudentController {
+
+	@Autowired
+	private CourseService cService;
+	@Autowired
+	private StudentService sService;
+	@Autowired
+	private EnrolmentService eService;
+// 	@Autowired
+//	private DepartmentValidator dValidator;
+	
+//	@RequestMapping(value = "/create", method = RequestMethod.GET)
+//	public ModelAndView newEmployeePage() {
+//		ModelAndView mav = new ModelAndView("employee-new", "employee", new ());
+//		mav.addObject("eidlist", cService.findAllEmployeeIDs());
+//		return mav;
+//	}
+	@RequestMapping(value="/sec",method = RequestMethod.GET)
+	public String testMestod(HttpServletRequest request){
+	    request.getSession().setAttribute("name", "S001");
+	    
+	    
+	    return "sec";
+	  }
+//	
+//	@SuppressWarnings("null")
+
+	@RequestMapping(value = "/a",method = RequestMethod.GET)
+	public ModelAndView AvailablePage1(HttpServletRequest request) {
 		
-		public class StudentController {
-			@Autowired
-			private EnrolmentService ens;
-			@Autowired
-			private CourseService crs;
-
-			@RequestMapping(value = "/sec", method = RequestMethod.GET)
-			public String testMestod(HttpServletRequest request) {
-				request.getSession().setAttribute("name", "S003");
-
-				return "Sessionset";
+		ModelAndView mav = new ModelAndView("course-available");
+		List<Course> courselist = cService.findAllCourses();
+		Object sid=request.getSession().getAttribute("name");
+		String s=(String) sid;
+		List<Enrolment> grades = eService.findCourseBySID(s);
+		
+		
+			for (Enrolment enrolment : grades) {
+				courselist.remove(enrolment.getCourses());
 			}
-
-			@RequestMapping(value = "/grade", method = RequestMethod.GET)
-			public ModelAndView studentviewgrade(HttpServletRequest request) {
-
-				ModelAndView mav = new ModelAndView("list-grade");
-				Object sid = request.getSession().getAttribute("name");
-				String s = (String) sid;
-				List<Enrolment> grades = ens.findCourseBySID(s);
-
-				mav.addObject("grlist", grades);
-				return mav;
-			}
-
-			@RequestMapping(value = "/course/view", method = RequestMethod.GET)
-			public ModelAndView studentviewcourse(HttpServletRequest request) {
-
-				ModelAndView mav = new ModelAndView("view-course");
-				Object sid = request.getSession().getAttribute("name");
-				String s = (String) sid;
-				ArrayList<Course> courses = crs.findbynotcop(s);
-
-				mav.addObject("crlist", courses);
-				return mav;
-			}
-
-			// ModelAndView mav = new ModelAndView("login");
-			// if (us.getSessionId() != null) {
-			// mav = new ModelAndView("/list-grade");
-			// mav.addObject("sgrade",
-			// eService.findCoursesByEID(us.getEmployee().getEmployeeId()));
-			// return mav;
-			// }
-			// return mav;
-
-
+		
+		
+		mav.addObject("courseavailable", courselist);
+		return mav;
 	}
+	
+///////////////////////////listall courses
+	
+	@RequestMapping(value = "/listall", method = RequestMethod.GET)
+	public ModelAndView CourseListPage(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView("list-all");
+		List<Course> courselist = cService.findAllCourses();
+		mav.addObject("courselist", courselist);
+		return mav;
+	}
+	
+
+	
+
+///////chunxiao need to refine this part
+
+
+	
+	@RequestMapping(value = "/grade",method = RequestMethod.GET)
+	public ModelAndView studentviewgrade(HttpServletRequest request) {
+		
+		ModelAndView mav = new ModelAndView("list-grade");
+		Object sid=request.getSession().getAttribute("name");
+		String s=(String) sid;
+		List<Enrolment> grades = eService.findCourseBySID(s);/////////joe changed in eservice
+		
+		mav.addObject("grlist", grades);
+		return mav;
+	}
+	
+	
+	
+	@RequestMapping(value = "/enrol/{courseId}", method = RequestMethod.GET)
+	public ModelAndView enrolling(@ModelAttribute Course course, BindingResult result,HttpServletRequest request,
+			@PathVariable int courseId, final RedirectAttributes redirectAttributes) {
+
+		   request.getSession().setAttribute("name", "S001");
+		
+		   Course c = cService.findCourse(courseId);
+		   String message ="";
+		
+
+		if (c.getSize()>c.getCurrentEnrollment()) {
+			Object sid=request.getSession().getAttribute("name");
+			String studentid=(String) sid;
+		    StudentDetail studentDetail =sService.findStudentById(studentid);
+  		    eService.createEnrollment(studentDetail, c);/////////need to check in eservice
+  		    cService.increasecourseEnrolment(c);/////////need to check in cservice
+  		    
+		    message = "You have sucessfully enrolled with NO. " + courseId + " course ";
+			redirectAttributes.addFlashAttribute("message", message);
+			
+			
+			
+			
+		}else {
+		    message = " NO. " + courseId + " course is aready full!!!";
+			redirectAttributes.addFlashAttribute("message", message);		
+			
+		}
+		request.getSession().setAttribute("message", message);
+//		Session session = SetAttribute("message", );
+		ModelAndView mav = new ModelAndView("redirect:/Course/bar");
+		
+		return mav;
+	}
+	@RequestMapping(value = "/bar",method = RequestMethod.GET)
+	public ModelAndView bar(HttpServletRequest request) {
+		
+		ModelAndView mav = new ModelAndView("bar");
+		String m=(String)request.getSession().getAttribute("message");
+	
+		
+		mav.addObject("message", m);
+		return mav;
+	}
+	
+		
+}
