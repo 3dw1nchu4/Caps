63929ace464954576ae8366fd2a61b6f9e1c9d6b
zssum <sumzhanseng@hotmail.com>
2016-12-11 17:38:07 +0800

    added hardcoded unauthenticated interceptor

--- Caps/src/main/java/edu/iss/caps/init/WebAppConfig.java
+++ Caps/src/main/java/edu/iss/caps/init/WebAppConfig.java
@@ -32,6 +32,7 @@ import org.springframework.web.servlet.view.JstlView;
 import edu.iss.caps.interceptor.AdminCheckInterceptor;
 import edu.iss.caps.interceptor.LecturerCheckInterceptor;
 import edu.iss.caps.interceptor.StudentCheckInterceptor;
+import edu.iss.caps.interceptor.UnauthenticatedInterceptor;
 // I am going to use SPRING CONTAINER
 @Configuration
 // Web MVC
@@ -150,6 +151,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
 		localeChangeInterceptor.setParamName("lang");
 		registry.addInterceptor(localeChangeInterceptor);
+		registry.addInterceptor(new UnauthenticatedInterceptor()).addPathPatterns("/admin","/admin/*","/admin/*/*","/Course","/Course/*","/Course/*/*","/Lec","/Lec/*","/Lec/*/*");
 		registry.addInterceptor(new AdminCheckInterceptor()).addPathPatterns("/admin/*","/admin/*/*");
 		registry.addInterceptor(new StudentCheckInterceptor()).addPathPatterns("/Course/*","/Course/*/*");
 		registry.addInterceptor(new LecturerCheckInterceptor()).addPathPatterns("/Lec/*","/Lec/*/*");
---
+++ Caps/src/main/java/edu/iss/caps/interceptor/UnauthenticatedInterceptor.java
@@ -0,0 +1,21 @@
+package edu.iss.caps.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class UnauthenticatedInterceptor extends HandlerInterceptorAdapter  {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+		throws Exception {		
+		
+		if(request.getSession().getAttribute("user")!=null)
+			response.sendRedirect("/caps/home/login");
+			
+		return true;
+		
+		
+	}
+}
+
