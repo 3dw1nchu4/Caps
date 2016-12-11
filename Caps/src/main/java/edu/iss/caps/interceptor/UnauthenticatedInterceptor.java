9d7cc2adaace2936374087152d378c1d58454c6d
zssum <sumzhanseng@hotmail.com>
2016-12-11 17:40:52 +0800

    revise unauthenticatedinterceptor

--- Caps/src/main/java/edu/iss/caps/interceptor/UnauthenticatedInterceptor.java
+++ Caps/src/main/java/edu/iss/caps/interceptor/UnauthenticatedInterceptor.java
@@ -10,7 +10,7 @@ public class UnauthenticatedInterceptor extends HandlerInterceptorAdapter  {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {		
 		
-		if(request.getSession().getAttribute("user")!=null)
+		if(request.getSession().getAttribute("user")==null)
 			response.sendRedirect("/caps/home/login");
 			
 		return true;
